927478d9228f80b75f06bbc9bfc2726126c47d13
Nathan Carpenter <nathan.carpenter03@gmail.com>
2018-02-11 14:47:15 -0500

    made firebase connection

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
 import React, { Component } from 'react';
-import database from './firebase.js'
 import './App.css';
+import { database } from './firebase';
 
 class App extends Component {
     constructor (props) {
         super (props);
         // set initial state of db
         this.state = {
-            data: null
+            data: null,
+            newData: ''
         }
+        // bind methods to current version of component
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    // react lifecycle methods
     componentDidMount() {
-        console.log ("I DID MOUNT")
+        console.log ("I did mount")
+        // database.ref(); - creates connection root node of the tree of json object
+        database.ref().on('value', (snapshot) => {
+            console.log('the data changed to:' + snapshot.val());
+            this.setState ({
+                data: snapshot.val()
+            })
+        } );
+        // Runs the arrow function whenever the db changes (console log in this case)
+        // Authorize read/write in firebase console -> db -> rules
+        // Use snapshot to listen for one single moment
     }
+
+    // component methods
+    handleChange(event){
+        const newData = event.target.value;
+        this.setState({
+            newData: newData
+            // or just newData (es6 shorthand for the above)
+        })
+    }
+    
+    handleSubmit(event) {
+        event.preventDefault();
+        const newData = database.ref().child('Amazing New Data').set(this.state.newData);
+
+    }
+
     render() {
         return (
             <div className="App">
                 <header className="App-header">
                     <h1 className="App-title">React And Firebase App</h1>
                 </header>
-                <pre className="App--data">
-                    One day, some data from Firebase will go here.
-        </pre>
+                <pre className="App-data">
+                    { JSON.stringify(this.state.data, null, 2)}
+                </pre>
+                <form className="App-form" onSubmit={this.handleSubmit}>
+                    <input type="text" value={this.state.newData} onChange={this.handleChange}/>
+                    <input type="submit" />
+                </form>
             </div>
         );
     }
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase';
 
-<script src="https://www.gstatic.com/firebasejs/4.9.1/firebase.js"></script>
+// <script src="https://www.gstatic.com/firebasejs/4.9.1/firebase.js"></script>
+
 
   const config = {
     apiKey: "AIzaSyBpAX_sqjotWqdllBdGmGU5wqGC4-ch8t4",
@@ -11,7 +12,10 @@ import firebase from 'firebase';
     messagingSenderId: "345604907102"
   };
   firebase.initializeApp(config);
+// Copy this code from firebase console -> auth -> web setup
 
   export default firebase;
 
   export const database = firebase.database();
+
+//   Export firebase db to use across the project from './firebase' instead of importing uninitialized sdk from 'firebase'
