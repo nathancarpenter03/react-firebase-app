bdb0fd41c9b2b5b0960b9fc017fb501d62febae7
Nathan Carpenter <nathan.carpenter03@gmail.com>
2018-02-12 21:24:03 -0500

    switched to data ref

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ class App extends Component {
             data: null,
             newData: ''
         }
+        // Set initial data ref
+        this.dataRef = null;
         // bind methods to current version of component
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -17,17 +19,27 @@ class App extends Component {
 
     // react lifecycle methods
     componentDidMount() {
+        
         console.log ("I did mount")
         // database.ref(); - creates connection root node of the tree of json object
-        database.ref().on('value', (snapshot) => {
+        // database.ref().on('value', (snapshot) => {
+        //     console.log('the data changed to:' + snapshot.val());
+        //     this.setState ({
+        //         data: snapshot.val()
+        //     })
+        // } );
+        // Runs the arrow function whenever the db changes (console log in this case)
+        // Authorize read/write in firebase console -> db -> rules
+        // Use snapshot to listen for one single moment
+
+        // If we are using data ref:
+        this.dataRef = database.ref('/wow/lol/hehe');
+        this.dataRef.on('value', (snapshot) => {
             console.log('the data changed to:' + snapshot.val());
             this.setState ({
                 data: snapshot.val()
             })
         } );
-        // Runs the arrow function whenever the db changes (console log in this case)
-        // Authorize read/write in firebase console -> db -> rules
-        // Use snapshot to listen for one single moment
     }
 
     // component methods
@@ -41,8 +53,12 @@ class App extends Component {
     
     handleSubmit(event) {
         event.preventDefault();
-        const newData = database.ref().child('Amazing New Data').set(this.state.newData);
+        // const newData = database.ref().child('AmazingNewData').push(this.state.newData); - or shorthand below
+        // const newData = database.ref('/AmazingNewData').push(this.state.newData);
+        // using push instead of key adds a randomized key for the new data
 
+        // Or, using data ref:
+        const newData = this.dataRef.push(this.state.newData);
     }
 
     render() {
